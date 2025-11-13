c4456140f483d6baad00f31a9c1e04fd6eda1b4f
Sridharon5 <malothsridharvarma@gmail.com>
2025-11-13 23:43:16 +0530

    Integrated getKeyPointIndicators APIs

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApiClient } from '../../services/api-client.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 @Component({
   selector: 'app-dashboard',
   imports: [CommonModule],
@@ -7,23 +9,45 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  stats = [
-    {
-      count: 2,
-      label: 'Total Departments',
-      icon: 'bi-clipboard',
-      color: 'blue',
-    },
-    {
-      count: 2,
-      label: 'Total Designations',
-      icon: 'bi-diagram-3',
-      color: 'green',
-    },
-    { count: 2, label: 'Total Users', icon: 'bi-person', color: 'yellow' },
-    { count: 2, label: 'Total Employees', icon: 'bi-people', color: 'purple' },
-    { count: 2, label: 'Total Evaluators', icon: 'bi-eye', color: 'violet' },
-    { count: 3, label: 'Total Tasks', icon: 'bi-alarm', color: 'cyan' },
-  ];
+export class DashboardComponent implements OnInit{
+  constructor(private api:ApiClient,private loader:NgxUiLoaderService){
+
+  }
+  stats:any[]=[];
+  ngOnInit(): void {
+   this.getKeyPointIndicators();
+  }
+  getKeyPointIndicators(){
+    this.loader.start();
+    this.api
+      .get('auth/getKeyPointIndicators')
+      .subscribe({
+        next: (res:any) => {
+          this.stats=res;
+          this.loader.stop();
+        },
+        error: (err) => {
+          console.error('Delete failed', err), 
+          this.loader.stop();
+        },
+      });
+  }
+  // stats = [
+  //   {
+  //     count: 2,
+  //     label: 'Total Departments',
+  //     icon: 'bi-clipboard',
+  //     color: 'blue',
+  //   },
+  //   {
+  //     count: 2,
+  //     label: 'Total Designations',
+  //     icon: 'bi-diagram-3',
+  //     color: 'green',
+  //   },
+  //   { count: 2, label: 'Total Users', icon: 'bi-person', color: 'yellow' },
+  //   { count: 2, label: 'Total Employees', icon: 'bi-people', color: 'purple' },
+  //   { count: 2, label: 'Total Evaluators', icon: 'bi-eye', color: 'violet' },
+  //   { count: 3, label: 'Total Tasks', icon: 'bi-alarm', color: 'cyan' },
+  // ];
 }
